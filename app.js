840e832880d88918fa459c1e3a50379ef3104d4e
Moaz Ayman <ma5746@fayoum.edu.eg>
2024-03-06 19:38:39 +0200

    Add ejs dependency and update view rendering

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render('404', { pageTitle: "404 Error", path: "/404"});
 });
 
 app.listen(3000);
